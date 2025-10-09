17e62f5ab2d9ca81c7a57da5edc896ef765500bb
Torben Neufeldt <torbenneufeldt@gmail.com>
2025-10-09 22:35:03 +0200

    Add initial biome config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import promise from 'eslint-plugin-promise';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import stylistic from '@stylistic/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -27,12 +26,10 @@ export default [{
   'eslint:recommended',
   'plugin:promise/recommended',
   'plugin:@typescript-eslint/strict',
-  'plugin:@typescript-eslint/stylistic',
 ), {
   plugins: {
     promise,
     '@typescript-eslint': typescriptEslint,
-    '@stylistic': stylistic,
   },
 
   languageOptions: {
@@ -46,102 +43,6 @@ export default [{
   },
 
   rules: {
-    '@stylistic/arrow-parens': 'off',
-    '@stylistic/array-bracket-spacing': ['error', 'never'],
-    '@stylistic/arrow-spacing': 'error',
-    '@stylistic/brace-style': 'warn',
-    '@stylistic/comma-dangle': ['error', 'always-multiline'],
-
-    '@stylistic/comma-spacing': ['error', {
-      before: false,
-      after: true,
-    }],
-
-    '@stylistic/computed-property-spacing': ['error', 'never'],
-    '@stylistic/eol-last': ['error', 'always'],
-    '@stylistic/keyword-spacing': 'error',
-    '@stylistic/key-spacing': 'error',
-
-    '@stylistic/max-len': ['error', {
-      code: 140,
-    }],
-
-    '@stylistic/no-multiple-empty-lines': ['error', {
-      max: 1,
-      maxEOF: 0,
-      maxBOF: 0,
-    }],
-
-    '@stylistic/no-multi-spaces': 'error',
-    '@stylistic/no-tabs': 'error',
-    '@stylistic/no-trailing-spaces': 'warn',
-    '@stylistic/object-curly-spacing': ['error', 'always'],
-    '@stylistic/padded-blocks': ['error', 'never'],
-
-    '@stylistic/padding-line-between-statements': ['error', {
-      blankLine: 'always',
-      prev: '*',
-      next: 'return',
-    }, {
-      blankLine: 'always',
-      prev: '*',
-      next: 'throw',
-    }, {
-      blankLine: 'always',
-
-      prev: [
-        'multiline-expression',
-        'multiline-block-like',
-        'multiline-const',
-        'multiline-let',
-      ],
-
-      next: 'continue',
-    }, {
-      blankLine: 'always',
-
-      prev: [
-        'multiline-expression',
-        'multiline-block-like',
-        'multiline-const',
-        'multiline-let',
-      ],
-
-      next: 'break',
-    }, {
-      blankLine: 'always',
-
-      prev: [
-        'multiline-expression',
-        'multiline-block-like',
-        'multiline-const',
-        'multiline-let',
-      ],
-
-      next: [
-        'multiline-expression',
-        'multiline-block-like',
-        'multiline-const',
-        'multiline-let',
-      ],
-    }],
-
-    '@stylistic/quote-props': ['error', 'as-needed'],
-    '@stylistic/quotes': ['error', 'single'],
-    '@stylistic/semi': ['error', 'always'],
-    '@stylistic/semi-spacing': 'error',
-    '@stylistic/space-before-blocks': 'error',
-
-    '@stylistic/space-before-function-paren': ['error', {
-      anonymous: 'never',
-      named: 'never',
-      asyncArrow: 'always',
-    }],
-
-    '@stylistic/space-in-parens': ['error', 'never'],
-    '@stylistic/spaced-comment': 'warn',
-    '@stylistic/indent': ['error', 2],
-
     '@typescript-eslint/array-type': ['error', {
       default: 'array',
       readonly: 'array',
